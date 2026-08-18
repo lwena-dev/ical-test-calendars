d22299be0c6e484050e173972e9439acdfc0112e
Lwena <lwena027@gmail.com>
2026-08-18 09:05:12 +0300

    Add AITAMA Hotel Room 101 iCal test calendar

--- threa.java
+++
@@ -1,5 +0,0 @@
-public class threa extends Thread {
-    public void run() {
-      System.out.println("This code is running in a thread");
-    }
-  }
--- threadimp.java
+++
@@ -1,6 +0,0 @@
-public class threadimp implements Runnable {
-  @Override
-  public void run() {
-    System.out.println("This code is running in a thread");
-  }
-}
